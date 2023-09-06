91a87a750806da6089588a5504f75443130231ee
pufick <98830871+pufick@users.noreply.github.com>
2023-09-06 21:13:54 +0300

    modified:   src/db/db.ts

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,13 +2,14 @@ import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 import "dotenv/config.js";
 
-const { SQL_PASSWORD, SQL_USER, SQL_HOST, DB } = process.env;
+const { SQL_PASSWORD, SQL_USER, SQL_HOST, DB, MYSQLPORT } = process.env;
 
 const mySqlConnectionsPool = mysql.createPool({
   host: SQL_HOST,
   user: SQL_USER,
   password: SQL_PASSWORD,
   database: DB,
+  port: Number(MYSQLPORT),
 });
 
 export const db = drizzle(mySqlConnectionsPool, { logger: true });
