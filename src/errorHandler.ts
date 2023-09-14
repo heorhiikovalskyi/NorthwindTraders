bf4c99b91243a45c52b118f2a995e40c2569f8c1
pufick <98830871+pufick@users.noreply.github.com>
2023-09-14 16:49:58 +0300

    modified:   src/errorHandler.ts

--- src/errorHandler.ts
+++ src/errorHandler.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 
-export const errorHandler = async (err: any, res: Response) => {
+export const errorHandler = async (err: any, req: Request, res: Response, next: any) => {
   console.log(err);
   return res.sendStatus(500);
 };
